62109c0746539bff1788b5cddd6add6dcf31596b
Minh Mẫn <pmmdrg2605@gmail.com>
2022-11-09 21:25:13 +0700

    Complete UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,10 @@ import React from "react";
 import Home from "./pages/homepage/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email address" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign in</button>
+          <span>
+            New to Netflix Clone? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
