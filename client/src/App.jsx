96bcbf4320d3dc9da15c125f8895f1f386823e7b
Pham Minh Man <pmmdrg2605@gmail.com>
2024-06-18 23:38:33 +0700

    update lib

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import "./App.scss";
+import './App.scss';
 
-import React from "react";
-import Home from "./pages/homepage/Home";
-import Watch from "./pages/watch/Watch";
-import Register from "./pages/register/Register";
-import Login from "./pages/login/Login";
+import React from 'react';
+import Home from './pages/homepage/Home';
+import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 const App = () => {
   return <Home />;
+  // return <Watch />;
+  // return <Register />;
+  // return <Login />;
 };
 
 export default App;
